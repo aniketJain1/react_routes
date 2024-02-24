30e7db039e44b2ab049f7b361aceb37ef4edf5b3
Aniket Jain <aniketkjain1@gmail.com>
2024-02-24 12:48:14 +0530

    useParams Hook

--- src/Components/UserDetails.js
+++ src/Components/UserDetails.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useParams } from 'react-router'
 
 function UserDetails() {
+
+  // const userId = useParams();
+  // const id = userId.id;
+  // console.log('id :',id);
+  // console.log(userId);
+  const { id } = useParams();
+
   return (
     <div>
-        <h2>User Details Page..</h2>
+        <h2>User Details Page -- {id}</h2>
     </div>
   )
 }
