7c3b6566ccce8e371c12db5824a0b3cc4d088196
Aniket Jain <aniketkjain1@gmail.com>
2024-02-27 21:49:08 +0530

    State Management & Validation Using Formik & Yup

---
+++ src/Components/FormikYup/FormSchema.js
@@ -0,0 +1,11 @@
+import * as Yup from 'yup';
+
+export const FormSchema = Yup.object({
+    // name:Yup.string().min(3).max(20).required("Name is Must"),
+    name:Yup.string().min(3,'Too Short').max(20,'Invalid Email').required("Name is Must"),
+    email:Yup.string().email().required("Email is Must"),
+    age:Yup.number().min(10).max(50).required('Age is Must'),
+    pass: Yup.string().required("Pass is Must").matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/, 'Enter Strong Password'),
+    cpass : Yup.string().required("Confirm Password is Must").oneOf([Yup.ref('pass'), null], 'Both Password Must Match'),
+
+});
--- src/Components/FormikYup/FormikForm.js
+++ src/Components/FormikYup/FormikForm.js
@@ -1,9 +1,18 @@
 import { Formik, useFormik } from "formik";
+import { FormSchema } from "./FormSchema";
 
 function FormikForm() {
   const formInitialValues = {
-    name: "Aniket",
-    email: "xyz",
+    // name: "Aniket",
+    // email: "xyz",
+    // age : '12'
+
+    name: '',
+    email: '',
+    age : '',
+    pass : '',
+    cpass : '',
+
   };
 
   // const formik = useFormik({
@@ -14,27 +23,36 @@ function FormikForm() {
   //       console.log(values.email);
   //     }
 
-  const {handleSubmit, handleChange, values} = useFormik({
+  const { handleSubmit, handleChange, values, errors, handleBlur, touched } = useFormik({
     initialValues: formInitialValues,
-    onSubmit: (values) => {
+    validationSchema: FormSchema ,
+    onSubmit: (values, action) => {
       console.log(values);
       console.log(values.name);
       console.log(values.email);
-    }
-
+      console.log(values.age);
+      console.log(values.pass);
+      console.log(values.cpass);
+      action.resetForm();
+    },
   });
 
   return (
     <div>
       <h1>Formik Demo</h1>
       <form onSubmit={handleSubmit}>
+
         <label htmlFor="">Enter Name : </label>
         <input
           type="text"
           name="name"
           onChange={handleChange}
           value={values.name}
+          onBlur={handleBlur}
         />
+        <br />
+        {/* <span style={{color:'red'}}>{errors.name}</span> */}
+        {errors.name && touched.name ? (<span style={{color:'red'}}>{errors.name}</span>) : null}
         <br />
         <br />
         <label htmlFor="">Enter Email : </label>
@@ -43,9 +61,58 @@ function FormikForm() {
           name="email"
           onChange={handleChange}
           value={values.email}
+          onBlur={handleBlur}
         />
         <br />
+        {/* <span style={{color:'red'}}>{errors.email}</span> */}
+        {errors.email && touched.email ? (<span style={{color:'red'}}>{errors.email}</span>) : null}
         <br />
+        <br />
+
+        <label htmlFor="">Enter Age : </label>
+        <input
+          type="text"
+          name="age"
+          onChange={handleChange}
+          value={values.age}
+          onBlur={handleBlur}
+        />
+        <br />
+        {/* <span style={{color:'red'}}>{errors.age}</span> */}
+        {errors.age && touched.age ? (<span style={{color:'red'}}>{errors.age}</span>) : null}
+        <br />
+        <br />
+
+        <label htmlFor="">Enter Password : </label>
+        <input
+          type="text"
+          // type="password"
+          name="pass"
+          onChange={handleChange}
+          value={values.pass}
+          onBlur={handleBlur}
+        />
+        <br />
+        {/* <span style={{color:'red'}}>{errors.pass}</span> */}
+        {errors.pass && touched.pass ? (<span style={{color:'red'}}>{errors.pass}</span>) : null}
+        <br />
+        <br />
+        
+        <label htmlFor="">Enter Confirm Password : </label>
+        <input
+          type="text"
+          // type="password"
+          name="cpass"
+          onChange={handleChange}
+          value={values.cpass}
+          onBlur={handleBlur}
+        />
+        <br />
+        {/* <span style={{color:'red'}}>{errors.cpass}</span> */}
+        {errors.cpass && touched.cpass ? (<span style={{color:'red'}}>{errors.cpass}</span>) : null}
+        <br />
+        <br />
+
         <input type="submit" value="Submit" />
       </form>
     </div>
